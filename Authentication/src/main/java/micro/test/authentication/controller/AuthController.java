df8c66a4795199f7b57f1f8e517f129dffec0517
Othmaneamrani <othmaneamrani31@gmail.com>
2024-03-06 01:40:58 +0100

    auth

--- Authentication/src/main/java/micro/test/authentication/controller/AuthController.java
+++ Authentication/src/main/java/micro/test/authentication/controller/AuthController.java
@@ -4,19 +4,25 @@ import lombok.AllArgsConstructor;
 import micro.test.authentication.entities.Produit;
 import micro.test.authentication.repository.IProduitRepository;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @AllArgsConstructor
 public class AuthController {
 
     private IProduitRepository iProduitRepository;
+    private ClientRegistrationRepository clientRegistrationRepository;
 
 
 
@@ -30,7 +36,7 @@ public class AuthController {
         return "notAuthorized";
     }
 
-
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/produits")
     public String produits (Model model){
         List<Produit> allProducts = iProduitRepository.findAll();
@@ -43,6 +49,20 @@ public class AuthController {
     public Authentication authentication (Authentication authentication){
         return authentication;
     }
+
+    @GetMapping("/oauth2login")
+    public String oauth2Login(Model model) {
+        String authorizationRequestBaseUri = "oauth2/authorization";
+        Map<String, String> oauth2AuthenticationUrls = new HashMap();
+        Iterable<ClientRegistration> clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;;
+        clientRegistrations.forEach(registration ->{
+            oauth2AuthenticationUrls.put(registration.getClientName(),
+                    authorizationRequestBaseUri + "/" + registration.getRegistrationId());
+        });
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+        return "oauth2Login";
+    }
+
 
 
 }
