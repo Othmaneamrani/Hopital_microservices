6ecc6114f655e75ec48110b0f9f5d0e82172c517
Othmaneamrani <othmaneamrani31@gmail.com>
2024-03-25 04:26:46 +0000

    spring admin

---
+++ SpringAdmin/src/test/java/micro/test/springadmin/SpringAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.test.springadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
